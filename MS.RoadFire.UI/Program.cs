75ae6e37a783c4a4adfc328a9cbd105b15118da9
Danielk862 <95142823+Danielk862@users.noreply.github.com>
2025-09-27 11:30:23 -0500

    The nuggets are installed

--- MS.RoadFire.UI/Program.cs
+++ MS.RoadFire.UI/Program.cs
@@ -24,10 +24,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
