cb10ade8f147a60651c9b210501dc9450503e121
guisaodani <guisaodani@gmail.com>
2025-09-27 22:18:15 -0500

    create IU home on page road fire

--- MS.RoadFire.UI/Program.cs
+++ MS.RoadFire.UI/Program.cs
@@ -4,22 +4,18 @@ using MS.RoadFire.CrossCutting.LocRegister;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MudBlazor services
 builder.Services.AddRegister();
 builder.Services.AddMudServices();
 builder.Services.AddServerSideBlazor().AddCircuitOptions(opt => { opt.DetailedErrors = true; });
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
