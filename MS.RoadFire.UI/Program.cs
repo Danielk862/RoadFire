50623a3f4ef48873439b6c0a8f97b406bec1b1b7
Isabel Rua <isabelrua19@gmail.com>
2025-09-29 00:06:18 -0500

    UI: integrar repositorio HTTP y Program.cs (consumo API)

--- MS.RoadFire.UI/Program.cs
+++ MS.RoadFire.UI/Program.cs
@@ -1,23 +1,22 @@
 using MS.RoadFire.UI.Components;
-using MS.RoadFire.CrossCutting.LocRegister;
-using MS.RoadFire.DataAccess.Context;
-using Microsoft.EntityFrameworkCore;
+using MS.RoadFire.UI.Repositories;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMudServices();
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7214/") });
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7214/") //  API
+});
+
+builder.Services.AddScoped<IRepository, Repository>();
+
 builder.Services.AddServerSideBlazor().AddCircuitOptions(opt => { opt.DetailedErrors = true; });
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-// Add DbContext with SQL Server provider
-var connectionString = builder.Configuration.GetConnectionString("RoadFireContext");
-
-builder.Services.AddDbContext<DbRoadFireContext>(options =>
-    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -30,6 +29,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAntiforgery();
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
